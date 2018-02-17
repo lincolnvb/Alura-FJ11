f292a792d36a1b525d83359c3ed4f232edfeeb9e
Lincoln Bassoto <lincolnvb@outlook.com>
2018-02-17 11:49:56 -0200

    Commit dos exercicios pt1

---
+++ ProgramaEx1.java
@@ -0,0 +1,6 @@
+class CodigoEx1 {
+	public static void main (String[] args){
+		System.out.println("Olá Caelum!!!");
+
+	}
+}		
---
+++ ProgramaEx2.java
@@ -0,0 +1,8 @@
+class CodigoEx2 {
+	public static void main (String[] args){
+		System.out.println("Olá mundo!");
+		System.out.println("Olá Caelum!");
+
+	}
+
+}
