d11cc06af6940930c1d3933a9f7e9cc7b8e2e7d7
Lincoln Bassoto <lincolnvb@outlook.com>
2018-02-17 14:11:47 -0200

    Adicionado a classe 'Variaveis'

--- FJ11/Variaveis.java
+++ FJ11/Variaveis.java
@@ -2,8 +2,15 @@ class Variaveis {
 	public static void main(String [] args){
 		int idade = 25;
 		int idadeNoAnoQueVem = idade+1;			
+		char letra = 'c';
+		double preco = 10/4;
+		boolean verdade = true;		
 
-	System.out.println("idade: " + idade);
+	System.out.println("Idade: " + idade);
+	System.out.println("Valor: " +preco+ " Reais.");
+	System.out.println("Letra: "+ letra);	
+	verdade = !verdade;
+	System.out.println("Estado: "+ verdade);
 }
 	
 }
